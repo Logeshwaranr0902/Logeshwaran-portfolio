8a8ad8a49afa3e2f3b8a23af47cf68ba1e7aa35e
LOGESHWARAN R <96953357+Logeshwaranr0902@users.noreply.github.com>
2024-10-12 13:49:50 +0530

    Added Scrollbar

--- src/sections/Middle/Projects.jsx
+++ src/sections/Middle/Projects.jsx
@@ -71,48 +71,50 @@ const Projects = () => {
   return (
     <section
       ref={sectionRef}
-      className={`min-h-fit pb-5 flex flex-col items-center text-yellow-300 bg-black`}
+      className={`max-h-screen pb-5 flex flex-col items-center text-yellow-300 bg-black`}
     >
       <p className="text-[50px] mt-[18px] font-bold mb-10">PROJECTS</p>
-      <div className="space-y-8 w-full max-w-4xl mb-6">
-        {projects.map((project, index) => (
-          <div
-            key={project.id}
-            className={`flex flex-col sm:flex-row items-center border border-white bg-black p-4 rounded-lg shadow-md px-4 sm:px-9 transition-transform duration-700 ${
-              isVisible
-                ? "transform translate-x-0 opacity-100"
-                : "transform -translate-x-full opacity-0"
-            }`}
-            style={{ transitionDelay: `${index * 200}ms` }}
-          >
-            <div className="flex-shrink-0 mb-4 sm:mb-0">
-              <a href={project.link} target="_blank">
-                <img
-                  src={project.image}
-                  alt={project.title}
-                  className="w-full h-auto border border-white hover:border-blue-400 sm:w-64 sm:h-36 object-contain rounded-md mr-8 transition-transform duration-300 ease-in-out transform hover:scale-110"
-                />
-              </a>
-            </div>
+      <div className="overflow-y-auto max-h-screen scrollbar-thin px-6 scrollbar-thumb-yellow-500 scrollbar-track-black">
+        <div className="space-y-8 w-full max-w-4xl mb-6">
+          {projects.map((project, index) => (
+            <div
+              key={project.id}
+              className={`flex flex-col sm:flex-row items-center border border-white bg-black p-4 rounded-lg shadow-md px-4 sm:px-9 transition-transform duration-700 ${
+                isVisible
+                  ? "transform translate-x-0 opacity-100"
+                  : "transform -translate-x-full opacity-0"
+              }`}
+              style={{ transitionDelay: `${index * 200}ms` }}
+            >
+              <div className="flex-shrink-0 mb-4 sm:mb-0">
+                <a href={project.link} target="_blank">
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    className="w-full h-auto border border-white hover:border-blue-400 sm:w-64 sm:h-36 object-contain rounded-md mr-8 transition-transform duration-300 ease-in-out transform hover:scale-110"
+                  />
+                </a>
+              </div>
 
-            <div>
-              <div className="flex flex-col justify-center text-left flex-grow">
-                <h3>
-                  <span className="text-xl inline-block font-semibold mb-2 text-yellow-300 hover:text-white">
-                    <a
-                      href={project.link2}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {project.title}
-                    </a>
-                  </span>
-                </h3>
-                <p className="text-gray-400 text-sm">{project.description}</p>
+              <div>
+                <div className="flex flex-col justify-center text-left flex-grow">
+                  <h3>
+                    <span className="text-xl inline-block font-semibold mb-2 text-yellow-300 hover:text-white">
+                      <a
+                        href={project.link2}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {project.title}
+                      </a>
+                    </span>
+                  </h3>
+                  <p className="text-gray-400 text-sm">{project.description}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 module.exports = {
   content: [
-    "./index.html",           // Vite's index.html
+    "./index.html", // Vite's index.html
     "./src/**/*.{js,jsx,ts,tsx}", // Scan your React files
   ],
   theme: {
     extend: {
       fontFamily: {
-        robotoMono: ['Roboto Mono', 'monospace'],
-        rubik: ['Rubik', 'sans-serif'],
-        quicksand: ['Quicksand', 'sans-serif'],
-        
+        robotoMono: ["Roboto Mono", "monospace"],
+        rubik: ["Rubik", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 };
